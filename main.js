edfa72305612a0e67fe8cffc3d1c6f0b3a3b9e7d
thamer <themerlefi@gmail.com>
2023-03-04 15:46:14 +0100

    end

--- main.js
+++ main.js
@@ -1,62 +1,81 @@
-let delBtns = document.querySelectorAll('.del')
+let items = [
+    {id:1 , name: 'item 1', price: '10', quantity: 0},
+    {id:2 , name: 'item 2', price: '20', quantity: 0},
+    {id:3 , name: 'item 3', price: '30', quantity: 0},
+    {id:4 , name: 'item 4', price: '40', quantity: 0},
+]
 
-    for (let i=0 ; i<delBtns.length ; i++ ){
+let container = document.getElementById('root')
+for(let i=0;i<items.length;i++){
+    container.innerHTML += `
+    <div class="cart">
+        <h5>${items[i].name}</h5>
+        <h5 class="price">${items[i].price}</h5>
+        <h5 class="quant minus">-</h5>
+        <h5 class="num">${items[i].quantity}</h5>
+        <h5 class="quant plus">+</h5>
+        <button class="del">delete</button>
+        <i class="fa-regular fa-heart"></i>
+    </div>
+    `
+}
 
-        let delBtn = delBtns[i]
-        
-        delBtn.addEventListener('click',function(event) {
-            let clicked = event.target
-            clicked.parentElement.remove()
-        })
-    }
+// remove element
 
-let pluss = document.querySelectorAll('.plus') 
-let minuss = document.querySelectorAll('.minus')   
-let virt = 0
+let deleteItems = document.querySelectorAll('.del')
+for(let i=0;i<deleteItems.length;i++){
+    let deleteItem = deleteItems[i]
+    deleteItem.addEventListener('click', (event) =>{
+        let targ = event.target
+        targ.parentElement.remove()
+    })
+}
+
+// like icons
+let likes = document.querySelectorAll('.cart i')
+for(let i=0;i<likes.length;i++){
+    let like = likes[i]
+    like.addEventListener('click',(event) =>{
+        let liked = event.target
+        liked.classList.toggle('love')
+    })
+}
+
+// increm quant
+let total = document.querySelector('.total');
+
+let pluss = document.querySelectorAll('.plus')
+let num = document.querySelectorAll('.num')
+let price = document.querySelectorAll('.price')
+let endTotal = 0
 for(let i=0;i<pluss.length;i++){
-    let plus = pluss[i];
-    plus.addEventListener('click', function(event){
-        let quantity = event.target
-        let inPlus = plus.previousElementSibling.innerText
-        inPlus++
-        quantity.previousElementSibling.innerHTML = inPlus
-        let price = document.querySelectorAll('.price')
-        let num = document.querySelectorAll('.num')
-        price[i].innerText *= num[i].innerText
-    })
-}
-for(let i=0;i<minuss.length;i++){
-    let minus = minuss[i]
-    minus.addEventListener('click', function(event){
-        let quantt = event.target
-        let inMinus = minus.nextElementSibling.innerText
-        if(inMinus>1){
-            inMinus--
-         quantt.nextElementSibling.innerHTML = inMinus
-         let price = document.querySelectorAll('.price')
-         let num = document.querySelectorAll('.num')
-         price[i].innerText = 10 * num[i].innerText
-        }
-
-    })
-}
-let love = document.querySelectorAll('.cart i')
-for(i=0;i<love.length;i++){
-    let lov = love[i]
-    lov.addEventListener('click', function(event){
-        let aim = event.target
-        aim.classList.toggle('love')
+    let prices = price[i].innerText
+    let plus = pluss[i]
+    plus.addEventListener('click', (event) =>{
+        let inc = event.target
+            inc.previousElementSibling.innerText++
+            price[i].innerText = prices * +inc.previousElementSibling.innerText
+            endTotal +=  +prices
+            total.innerText = `Total $${endTotal}`
+        })
         
+}
+
+// decrem quant
+
+let minuss = document.querySelectorAll('.minus')
+for(let i=0;i<minuss.length;i++){
+    let prices = price[i].innerText
+    let minus = minuss[i]
+    minus.addEventListener('click', (event) =>{
+       if (minus.nextElementSibling.innerText >0){
+        let inc = event.target
+            inc.nextElementSibling.innerText--
+            price[i].innerText = prices * +inc.nextElementSibling.innerText
+            endTotal -=  +prices
+            total.innerText = `Total $${endTotal}`
+       }
     })
 }
 
-
-
-
-
-
-
-
-
-
-
+//total
